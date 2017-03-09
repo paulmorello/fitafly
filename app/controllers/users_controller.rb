1f3401811313ca3f6cec85eeb8485fbb7a8480bb
paulmorello <paulmorello1@gmail.com>
2017-03-08 19:46:19 -0500

    added form validations to avoid duplicate data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,16 +7,17 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new
-    user.username = params[:username]
-    user.email = params[:email]
-    user.password = params[:password]
+    @user = User.new
+    @user.username = params[:username]
+    @user.email = params[:email]
+    @user.password = params[:password]
 
-    if user.save
-      session[:user_id] = user.id
+    if @user.save
+      session[:user_id] = @user.id
 
       redirect_to '/events'
     else
+
       render :new
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,24 @@ class User < ApplicationRecord
   has_secure_password
   has_many :events
 
+  # Validation constraints
+  validates :email, :username, presence: true
+  validates :email, uniqueness: true
+
+  validates :username, uniqueness: true, length: {
+    in: 4..400,
+    },
+    format: {
+      without: /[^a-z0-9]/i,
+      message: "no special characters or spacing"
+    },
+    exclusion: {
+      :in => %w(login signup settings add api snippets search browse archive),
+      message: "has already been taken"
+    }
+  validates :email,
+    format: {
+      with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+    }
+
 end
