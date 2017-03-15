c31258a08d6b16fd4e09f9887178fb1f1ba99984
paulmorello <paulmorello1@gmail.com>
2017-03-15 19:54:10 -0400

    added date column back into event table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170315233147) do
+ActiveRecord::Schema.define(version: 20170315235326) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20170315233147) do
     t.string   "difficulty"
     t.string   "additional_information"
     t.decimal  "cost"
+    t.string   "date"
   end
 
   create_table "registrations", force: :cascade do |t|
