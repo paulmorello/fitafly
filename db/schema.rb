95fec70d546e3a028588687b23593353a99905c5
paulmorello <paulmorello1@gmail.com>
2017-04-23 16:47:32 -0400

    message table created for chat feature, along with model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170423204114) do
+ActiveRecord::Schema.define(version: 20170423204454) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,14 @@ ActiveRecord::Schema.define(version: 20170423204114) do
     t.string   "additional_information"
     t.decimal  "cost"
     t.string   "date"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.string   "body"
+    t.integer  "chatroom_id"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "registrations", force: :cascade do |t|
