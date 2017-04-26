fea6e34cb8931ef0c103fe82c0f4a738b5615458
paulmorello <paulmorello1@gmail.com>
2017-04-25 20:53:17 -0400

    added views for messages and fixing scope between sender and recipient bug

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,5 +1,4 @@
 class ChatroomsController < ApplicationController
-  before_action :authenticate_user
 
   def index
     @users = User.all
--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -10,7 +10,7 @@ class Chatroom < ApplicationRecord
 
   # Checks the sender id and the recipient id and checks if a relationship exists between them
   scope :between, -> (sender_id,recipient_id) do
-    where(“(chatrooms.sender_id = ? AND chatrooms.recipient_id =?) OR (cchatrooms.sender_id = ? AND cchatrooms.recipient_id =?)”, sender_id,recipient_id, recipient_id, sender_id)
+    where(“(chatrooms.sender_id = ? AND chatrooms.recipient_id =?) OR (chatrooms.sender_id = ? AND chatrooms.recipient_id =?)”, sender_id,recipient_id, recipient_id, sender_id)
   end
 
 end
