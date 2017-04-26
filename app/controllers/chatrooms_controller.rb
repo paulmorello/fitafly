fa15512765d1d556555056562550aa240eda5c38
paulmorello <paulmorello1@gmail.com>
2017-04-25 20:04:08 -0400

    Updates chatrooms controller, checks and creates conversations between 2 users

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,8 +1,27 @@
 class ChatroomsController < ApplicationController
+  before_action :authenticate_user
 
-  def show
-    @chatroom = Chatroom.find_by(slug: params[:slug])
-    @message = Message.new
+  def index
+    @users = User.all
+    @chatrooms = Chatroom.all
   end
-  
+
+  def create
+
+    if Chatroom.between(params[:sender_id],params[:recipient_id]).present?
+      @chatroom = Chatroom.between(params[:sender_id], params[:recipient_id]).first
+    else
+      @chatroom = Chatroom.create!(chatroom_params)
+    end
+
+    redirect_to chatroom_messages_path(@chatroom)
+
+  end
+
+  private
+
+    def chatroom_params
+      params.permit(:sender_id, :recipient_id)
+    end
+
 end
