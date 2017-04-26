9ddce12b413c1b362ba8186ad6b19e6f88b14270
paulmorello <paulmorello1@gmail.com>
2017-04-25 20:19:25 -0400

    updated messages controller to fix errors

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,21 +1,45 @@
 class MessagesController < ApplicationController
+  before_action do
+    @chatroom = Chatroom.find(params[:chatroom_id])
+  end
+
+  def index
+    @messages = @chatroom.messages
+
+    if @messages.length > 10
+      @over_ten = true
+      @messages = @messages[-10..-1]
+    end
+
+    if params[:m]
+      @over_ten = false
+      @messages = Chatroom.messages
+    end
+
+    if @messages.last
+      if @messages.last.user_id != current_user.id
+        @messages.last.read = true;
+      end
+    end
+
+    @message = @chatroom.messages.new
+  end
+
+  def new
+    @message = Chatroom.messages.new
+  end
 
   def create
-    message = Message.new(message_params)
-    message.user = current_user
-    if message.save
-      ActionCable.server.broadcast 'messages',
-        message: message.body,
-        user: message.user.username
-      head :ok
-    else
-      redirect_to chatrooms_path
+    @message = @conversation.messages.new(message_params)
+    if @message.save
+      redirect_to conversation_messages_path(@conversation)
     end
   end
 
   private
 
     def message_params
-      params.require(:message).permit(:content, :chatroom_id)
+      params.require(:message).permit(:body, :user_id)
     end
+
 end
