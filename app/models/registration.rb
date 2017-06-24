ba740fedaaabf32e763232427b319d2d8ea6de87
paulmorello <paulmorello1@gmail.com>
2017-06-24 10:32:28 -0700

    added validations to make sure users cant register for own event

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -2,8 +2,18 @@ class Registration < ApplicationRecord
   belongs_to :users, optional: true
   belongs_to :events, optional: true
 
+  before_create :user_cant_register_for_own_event
+
   # Make sure that one user cannot join the same event more than once at a time.
   validates :event_id, :uniqueness => { :scope => :user_id }
 
   validates :event_id, :user_id, presence: true
+
+  private
+    def user_cant_register_for_own_event
+      if :event_id == :user_id
+        false
+      end
+      true
+    end
 end
